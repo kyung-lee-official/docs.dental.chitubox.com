5dfc2ac31629b1fe78741b26b98b6162852a82d3
Kyung Lee <kyung.lee.official@outlook.com>
2024-11-26 11:07:55 +0800

    feat: add ZoomableImage

--- src/components/imageContainer/BlogImageContainer.tsx
+++ src/components/imageContainer/BlogImageContainer.tsx
@@ -1,3 +1,5 @@
+import { ZoomableImage } from "../zoomableImage/ZoomableImage";
+
 export const BlogImageContainer = (props: any) => {
 	const { src, alt, figcaption, style } = props;
 	return (
@@ -5,12 +7,7 @@ export const BlogImageContainer = (props: any) => {
 			className={`flex flex-col items-center w-full my-4 gap-2
             drop-shadow`}
 		>
-			<img
-				src={src}
-				alt={alt ?? ""}
-				style={style}
-				className="rounded-md overflow-hidden"
-			/>
+			<ZoomableImage src={src} alt={alt ?? ""} width={style.width} />
 			{figcaption && (
 				<figcaption className="text-center text-sm text-gray-500">
 					{figcaption}
