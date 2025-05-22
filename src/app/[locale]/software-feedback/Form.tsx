ad227b95a4c23f1bf03b9969c761d8af0df7634b
Kyung Lee <kyung.lee.official@outlook.com>
2025-05-22 13:47:10 +0800

    feat: enhance Form component with improved state management and validation schemas

--- src/app/[locale]/software-feedback/Form.tsx
+++ src/app/[locale]/software-feedback/Form.tsx
@@ -1,116 +1,257 @@
 "use client";
 
-import { Dropdown } from "@/components/universal-dropdown/dropdown/Dropdown";
-import { Country, FormType, formTypes } from "./form-options";
+import {
+	Dropdown,
+	DropdownOption,
+} from "@/components/universal-dropdown/dropdown/Dropdown";
+import {
+	bugReportingSchemaLoose,
+	Country,
+	FormState,
+	FormStateLoose,
+	FormTypeLiteral,
+	formTypes,
+	formTypesEnUS,
+	formTypesZhCN,
+	orderAndAccountIssuesSchemaLoose,
+	otherSchemaLoose,
+	suggestionsSchemaLoose,
+	usageHelpSchemaLoose,
+} from "./form-options";
 import { useReducer } from "react";
 import { IntegerInput } from "@/components/input/integer-input/IntegerInput";
 import { z } from "zod";
 import { countries } from "./countries";
+import { useLocale } from "next-intl";
 
-type State = {
-	country: Country | Country[] | null;
-	formType: FormType | FormType[] | null;
-
-	softwareVersion?: string;
-	computerSpecs: {
-		os: string | null;
-		cpu: string | null;
-		gpu: string | null;
-		gpuDriverVersion: string | null;
-		ramVolume: number | null;
-		storageVolume: number | null;
-	};
-	description: string | null;
+const initialState: FormStateLoose = {
+	name: "",
+	email: "",
+	country: null,
+	attachments: [],
+	dedicatedFields: null,
 };
 
-const initialState = {
-	country: null as Country | Country[] | null,
-	formType: null as FormType | FormType[] | null,
-	softwareVersion: "",
-	computerSpecs: {
-		os: "",
-		cpu: "",
-		gpu: "",
-		gpuDriverVersion: "",
-		ramVolume: null,
-		storageVolume: null,
-	},
-	description: "",
-};
+type OrderAndAccountIssues = z.infer<typeof orderAndAccountIssuesSchemaLoose>;
+type BugReporting = z.infer<typeof bugReportingSchemaLoose>;
+type UsageHelp = z.infer<typeof usageHelpSchemaLoose>;
+type Suggestions = z.infer<typeof suggestionsSchemaLoose>;
+type Other = z.infer<typeof otherSchemaLoose>;
 
 type Action =
-	| { type: "SET_COUNTRY"; payload: Country | Country[] | null }
-	| { type: "SET_FORM_TYPE"; payload: FormType | FormType[] | null }
-	| { type: "SET_SOFTWARE_VERSION"; payload: string }
-	| { type: "SET_COMPUTER_SPECS_OS"; payload: string }
-	| { type: "SET_COMPUTER_SPECS_CPU"; payload: string }
-	| { type: "SET_COMPUTER_SPECS_GPU"; payload: string }
-	| { type: "SET_COMPUTER_SPECS_GPU_DRIVER_VERSION"; payload: string }
-	| { type: "SET_COMPUTER_SPECS_RAM_VOLUME"; payload: number }
-	| { type: "SET_COMPUTER_SPECS_STORAGE_VOLUME"; payload: number };
+	/* common */
+	| { type: "SET_NAME"; payload: FormState["name"] }
+	| { type: "SET_EMAIL"; payload: FormState["email"] }
+	| { type: "SET_COUNTRY"; payload: DropdownOption }
+	| { type: "SET_ATTACHMENTS"; payload: string[] }
+	/* dedicated */
+	| {
+			type: "SET_FORM_TYPE";
+			payload: DropdownOption;
+	  }
+	| { type: "ORDER_ID"; payload: OrderAndAccountIssues["orderId"] }
+	| { type: "SET_COMPUTER_SPECS_OS"; payload: BugReporting["os"] }
+	| { type: "SET_SOFTWARE_VERSION"; payload: BugReporting["softwareVersion"] }
+	| { type: "SET_COMPUTER_SPECS_CPU"; payload: BugReporting["cpu"] }
+	| { type: "SET_COMPUTER_SPECS_GPU"; payload: BugReporting["gpu"] }
+	| {
+			type: "SET_COMPUTER_SPECS_GPU_DRIVER_VERSION";
+			payload: BugReporting["gpuDriverVersion"];
+	  }
+	| {
+			type: "SET_COMPUTER_SPECS_RAM_VOLUME";
+			payload: BugReporting["ramVolume"];
+	  }
+	| {
+			type: "SET_COMPUTER_SPECS_STORAGE_VOLUME";
+			payload: BugReporting["storageVolume"];
+	  }
+	| { type: "SET_DESCRIPTION"; payload: string };
 
-const reducer = (state: State, action: Action): State => {
+const reducer = (state: FormStateLoose, action: Action): FormStateLoose => {
 	switch (action.type) {
+		case "SET_NAME":
+			return { ...state, name: action.payload as string };
+		case "SET_EMAIL":
+			return { ...state, email: action.payload as string };
 		case "SET_COUNTRY":
-			return { ...state, country: action.payload };
+			return {
+				...state,
+				country: action.payload as {
+					id: string;
+					value: string;
+				},
+			};
 		case "SET_FORM_TYPE":
-			return { ...state, formType: action.payload };
-		case "SET_SOFTWARE_VERSION":
-			return { ...state, softwareVersion: action.payload };
+			let newDedicatedFields;
+			switch (action.payload.id) {
+				case "ORDER_AND_ACCOUNT_ISSUES":
+					newDedicatedFields = {
+						formType: { id: "ORDER_AND_ACCOUNT_ISSUES" },
+						orderId: "",
+						description: "",
+					} as const;
+					break;
+				case "BUG_REPORTING":
+					newDedicatedFields = {
+						formType: { id: "BUG_REPORTING" },
+						os: null,
+						softwareVersion: "",
+						cpu: null,
+						gpu: null,
+						gpuDriverVersion: null,
+						ramVolume: null,
+						storageVolume: null,
+						description: "",
+					} as const;
+					break;
+				case "USAGE_HELP":
+					newDedicatedFields = {
+						formType: { id: "USAGE_HELP" },
+						softwareVersion: "",
+						description: "",
+					} as const;
+					break;
+				case "SUGGESTIONS":
+					newDedicatedFields = {
+						formType: { id: "SUGGESTIONS" },
+						description: "",
+					} as const;
+					break;
+				case "OTHER":
+					newDedicatedFields = {
+						formType: { id: "OTHER" },
+						description: "",
+					} as const;
+					break;
+				default:
+					newDedicatedFields = state.dedicatedFields;
+			}
+			return {
+				...state,
+				dedicatedFields: newDedicatedFields,
+			};
+		case "ORDER_ID":
+			if (
+				state.dedicatedFields?.formType?.id ===
+				"ORDER_AND_ACCOUNT_ISSUES"
+			) {
+				return {
+					...state,
+					dedicatedFields: {
+						...state.dedicatedFields,
+						orderId: action.payload as string,
+					},
+				};
+			}
 		case "SET_COMPUTER_SPECS_OS":
-			return {
-				...state,
-				computerSpecs: {
-					...state.computerSpecs,
-					os: action.payload,
-				},
-			};
+			if (state.dedicatedFields?.formType?.id === "BUG_REPORTING") {
+				return {
+					...state,
+					dedicatedFields: {
+						...state.dedicatedFields,
+						os: action.payload as string,
+					},
+				};
+			}
+		case "SET_SOFTWARE_VERSION":
+			if (state.dedicatedFields?.formType?.id === "BUG_REPORTING") {
+				return {
+					...state,
+					dedicatedFields: {
+						...state.dedicatedFields,
+						softwareVersion: action.payload as string,
+					},
+				};
+			}
+
 		case "SET_COMPUTER_SPECS_CPU":
-			return {
-				...state,
-				computerSpecs: {
-					...state.computerSpecs,
-					cpu: action.payload,
-				},
-			};
+			if (state.dedicatedFields?.formType?.id === "BUG_REPORTING") {
+				return {
+					...state,
+					dedicatedFields: {
+						...state.dedicatedFields,
+						cpu: action.payload as string,
+					},
+				};
+			}
 		case "SET_COMPUTER_SPECS_GPU":
-			return {
-				...state,
-				computerSpecs: {
-					...state.computerSpecs,
-					gpu: action.payload,
-				},
-			};
+			if (state.dedicatedFields?.formType?.id === "BUG_REPORTING") {
+				return {
+					...state,
+					dedicatedFields: {
+						...state.dedicatedFields,
+						gpu: action.payload as string,
+					},
+				};
+			}
 		case "SET_COMPUTER_SPECS_GPU_DRIVER_VERSION":
-			return {
-				...state,
-				computerSpecs: {
-					...state.computerSpecs,
-					gpuDriverVersion: action.payload,
-				},
-			};
+			if (state.dedicatedFields?.formType?.id === "BUG_REPORTING") {
+				return {
+					...state,
+					dedicatedFields: {
+						...state.dedicatedFields,
+						gpuDriverVersion: action.payload as string,
+					},
+				};
+			}
+
 		case "SET_COMPUTER_SPECS_RAM_VOLUME":
-			return {
-				...state,
-				computerSpecs: {
-					...state.computerSpecs,
-					ramVolume: action.payload,
-				},
-			};
+			if (state.dedicatedFields?.formType?.id === "BUG_REPORTING") {
+				return {
+					...state,
+					dedicatedFields: {
+						...state.dedicatedFields,
+						ramVolume: action.payload as number,
+					},
+				};
+			}
 		case "SET_COMPUTER_SPECS_STORAGE_VOLUME":
-			return {
-				...state,
-				computerSpecs: {
-					...state.computerSpecs,
-					storageVolume: action.payload,
-				},
-			};
+			if (state.dedicatedFields?.formType?.id === "BUG_REPORTING") {
+				return {
+					...state,
+					dedicatedFields: {
+						...state.dedicatedFields,
+						storageVolume: action.payload as number,
+					},
+				};
+			}
 		default:
 			return state;
 	}
 };
 
 export const Form = () => {
+	const locale = useLocale();
+	let formTypeList;
+	let countryList: DropdownOption[];
+	switch (locale) {
+		case "en-US":
+			formTypeList = formTypesEnUS;
+			countryList = countries
+				.map((c) => ({
+					id: c.code,
+					value: c.name,
+				}));
+			break;
+		case "zh-CN":
+			formTypeList = formTypesZhCN;
+			countryList = countries
+				.map((c) => ({
+					id: c.code,
+					value: c.nameZhCn,
+				}));
+			break;
+		default:
+			formTypeList = formTypesEnUS;
+			countryList = countries
+				.map((c) => ({
+					id: c.code,
+					value: c.name,
+				}));
+			break;
+	}
+
 	const [state, dispatch] = useReducer(reducer, initialState);
 
 	return (
@@ -154,47 +295,135 @@ export const Form = () => {
 						{/* region */}
 						<div className="space-y-1">
 							<div>Country/Region</div>
-							<Dropdown<Country>
-								kind="object"
-								sortBy="code"
-								label={{ primaryKey: "name" }}
+							<Dropdown
 								mode="search"
-								options={countries}
+								options={countryList}
 								selected={state.country}
 								setSelected={(value) =>
 									dispatch({
 										type: "SET_COUNTRY",
-										payload: value as
-											| Country
-											| Country[]
-											| null,
+										payload: value as DropdownOption,
 									})
 								}
 								placeholder="Please select your country or region"
+								getLabel={(option) => {
+									const country = countries.find(
+										(item) => item.code === option.id
+									);
+									return country?.name as string;
+								}}
+								renderOption={(option, { selected }) => {
+									const country = countries.find(
+										(item) => item.code === option.id
+									);
+									return (
+										<div
+											className={`px-2 py-1
+											hover:text-blue-500
+											hover:bg-neutral-100
+											rounded-md cursor-pointer`}
+										>
+											<span>{country?.name}</span>
+										</div>
+									);
+								}}
+								controlClassName="flex items-center flex-wrap w-full h-12 px-4 py-1.5 gap-2
+								bg-neutral-100
+								rounded-md cursor-pointer"
+								selectedItemClassName={(option) => {
+									return `text-black truncate`;
+								}}
+								placeholderClassName="text-neutral-500/90 text-base truncate"
+								menuClassName="absolute z-10 w-full mt-1 p-2.5
+								bg-white
+								rounded-md shadow-lg overflow-auto"
+								searchInputClassName="w-full p-2 border-b border-neutral-200 outline-none"
+								optionClassName={(
+									option,
+									{ selected, hovered }
+								) => {
+									return `px-2 py-1
+									hover:text-blue-500
+									hover:bg-neutral-100
+									rounded-md cursor-pointer`;
+								}}
 							/>
 						</div>
 						{/* type */}
 						<div className="space-y-1">
 							<div>What is your issue about?</div>
-							<Dropdown<FormType>
-								kind="object"
-								label={{ primaryKey: "value" }}
+							<Dropdown
 								mode="regular"
 								options={formTypes}
-								selected={state.formType}
+								selected={
+									state.dedicatedFields
+										?.formType as DropdownOption
+								}
 								setSelected={(value) =>
 									dispatch({
 										type: "SET_FORM_TYPE",
-										payload: value as
-											| FormType
-											| FormType[]
-											| null,
+										payload: value as DropdownOption,
 									})
 								}
 								placeholder="Please select your feedback type (required)"
+								getLabel={(option) => {
+									const formType = formTypeList.find(
+										(item) => item.id === option.id
+									);
+									return formType?.value as string;
+								}}
+								renderOption={(option, { selected }) => {
+									const formType = formTypeList.find(
+										(item) => item.id === option.id
+									);
+									return (
+										<div
+											className={`px-2 py-1
+											hover:text-blue-500
+											hover:bg-neutral-100
+											rounded-md cursor-pointer`}
+										>
+											<span>{formType?.value}</span>
+										</div>
+									);
+								}}
+								controlClassName="flex items-center flex-wrap w-full h-12 px-4 py-1.5 gap-2
+								bg-neutral-100
+								rounded-md cursor-pointer"
+								selectedItemClassName={(option) => {
+									return `text-black truncate`;
+								}}
+								placeholderClassName="text-neutral-500/90 text-base truncate"
+								menuClassName="absolute z-10 w-full mt-1 p-2.5
+								bg-white
+								rounded-md shadow-lg overflow-auto"
+								searchInputClassName="w-full p-2 border-b border-neutral-200 outline-none"
+								optionClassName={(
+									option,
+									{ selected, hovered }
+								) => {
+									return `px-2 py-1
+									hover:text-blue-500
+									hover:bg-neutral-100
+									rounded-md cursor-pointer`;
+								}}
 							/>
 						</div>
-						{(state.formType as FormType)?.key ===
+						{(state.dedicatedFields?.formType as DropdownOption)
+							?.id === "ORDER_AND_ACCOUNT_ISSUES" && (
+							<div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+								<div className="space-y-1">
+									<div>Order Id</div>
+									<input
+										type="text"
+										placeholder="Your CHITUBOX Order ID (optional)"
+										className="block w-full h-12 px-4 py-1.5
+										bg-neutral-100 rounded-md outline-none"
+									/>
+								</div>
+							</div>
+						)}
+						{/* {(state.formType as FormTypeOption)?.key ===
 							"BUG_REPORTING" && (
 							<div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
 								<div className="space-y-1">
@@ -257,7 +486,7 @@ export const Form = () => {
 									/>
 								</div>
 							</div>
-						)}
+						)} */}
 						{/* description */}
 						<textarea
 							className="w-full h-40 px-4 py-1.5
--- src/app/[locale]/software-feedback/form-options.ts
+++ src/app/[locale]/software-feedback/form-options.ts
@@ -1,32 +1,37 @@
+import { DropdownOption } from "@/components/universal-dropdown/dropdown/Dropdown";
 import { z } from "zod";
 
-export type FormType = {
-	key: string;
-	value: string;
-};
-
-export const formTypes: FormType[] = [
-	{ key: "ORDER_AND_ACCOUNT_ISSUES", value: "Order and Account Issues" },
-	{ key: "BUG_REPORTING", value: "Bug Reporting" },
-	{ key: "USAGE_HELP", value: "Usage Help" },
-	{ key: "SUGGESTIONS", value: "Suggestions" },
-	{ key: "OTHER", value: "Other" },
+export const formTypes: DropdownOption[] = [
+	{ id: "ORDER_AND_ACCOUNT_ISSUES" },
+	{ id: "BUG_REPORTING" },
+	{ id: "USAGE_HELP" },
+	{ id: "SUGGESTIONS" },
+	{ id: "OTHER" },
 ];
 
-const formSchema = z.object({
-	country: z.any().nullable(),
-	formType: z.any().nullable(),
-	softwareVersion: z.string().optional(),
-	computerSpecs: z.object({
-		os: z.string().nullable(),
-		cpu: z.string().nullable(),
-		gpu: z.string().nullable(),
-		gpuDriverVersion: z.string().nullable(),
-		ramVolume: z.number().nullable(),
-		storageVolume: z.number().nullable(),
-	}),
-	description: z.string().nullable(),
-});
+export const formTypesEnUS = [
+	{ id: "ORDER_AND_ACCOUNT_ISSUES", value: "Order and Account Issues" },
+	{ id: "BUG_REPORTING", value: "Bug Reporting" },
+	{ id: "USAGE_HELP", value: "Usage Help" },
+	{ id: "SUGGESTIONS", value: "Suggestions" },
+	{ id: "OTHER", value: "Other" },
+];
+export const formTypesZhCN = [
+	{ id: "ORDER_AND_ACCOUNT_ISSUES", value: "订单和账户问题" },
+	{ id: "BUG_REPORTING", value: "错误报告" },
+	{ id: "USAGE_HELP", value: "使用帮助" },
+	{ id: "SUGGESTIONS", value: "建议" },
+	{ id: "OTHER", value: "其他" },
+];
+
+export const formTypeSchema = z.union([
+	z.literal("ORDER_AND_ACCOUNT_ISSUES"),
+	z.literal("BUG_REPORTING"),
+	z.literal("USAGE_HELP"),
+	z.literal("SUGGESTIONS"),
+	z.literal("OTHER"),
+]);
+export type FormType = z.infer<typeof formTypeSchema>;
 
 const countrySchema = z.object({
 	code: z.string(),
@@ -34,3 +39,113 @@ const countrySchema = z.object({
 	nameZhCn: z.string(),
 });
 export type Country = z.infer<typeof countrySchema>;
+
+const commonFields = {
+	name: z.string().min(1).max(50),
+	email: z.string().email(),
+	country: countrySchema,
+	attachments: z.array(z.string()),
+};
+
+export const orderAndAccountIssuesSchema = z.object({
+	formType: z.literal("ORDER_AND_ACCOUNT_ISSUES"),
+	orderId: z.string().min(1),
+	description: z.string(),
+});
+
+export const bugReportingSchema = z.object({
+	formType: z.literal("BUG_REPORTING"),
+	os: z.string(),
+	softwareVersion: z.string(),
+	cpu: z.string(),
+	gpu: z.string(),
+	gpuDriverVersion: z.string(),
+	ramVolume: z.number(),
+	storageVolume: z.number(),
+	description: z.string(),
+});
+
+export const usageHelpSchema = z.object({
+	formType: z.literal("USAGE_HELP"),
+	softwareVersion: z.string(),
+	description: z.string(),
+});
+
+export const suggestionsSchema = z.object({
+	formType: z.literal("SUGGESTIONS"),
+	description: z.string(),
+});
+
+export const otherSchema = z.object({
+	formType: z.literal("OTHER"),
+	description: z.string(),
+});
+
+const formSchema = z.object({
+	...commonFields,
+	dedicatedFields: z.discriminatedUnion("formType", [
+		orderAndAccountIssuesSchema,
+		bugReportingSchema,
+		usageHelpSchema,
+		suggestionsSchema,
+		otherSchema,
+	]),
+});
+
+export type FormState = z.infer<typeof formSchema>;
+export type FormTypeLiteral = FormState["dedicatedFields"]["formType"];
+
+const commonFieldsLoose = {
+	name: z.string().min(1).max(50).nullable(),
+	email: z.string().email().nullable(),
+	country: z.object({ id: z.string(), value: z.string() }).nullable(),
+	attachments: z.array(z.string()),
+};
+
+export const orderAndAccountIssuesSchemaLoose = z.object({
+	formType: z.object({ id: z.literal("ORDER_AND_ACCOUNT_ISSUES") }),
+	orderId: z.string().min(1).nullable(),
+	description: z.string().nullable(),
+});
+
+export const bugReportingSchemaLoose = z.object({
+	formType: z.object({ id: z.literal("BUG_REPORTING") }),
+	os: z.string().nullable(),
+	softwareVersion: z.string().nullable(),
+	cpu: z.string().nullable(),
+	gpu: z.string().nullable(),
+	gpuDriverVersion: z.string().nullable(),
+	ramVolume: z.number().nullable(),
+	storageVolume: z.number().nullable(),
+	description: z.string().nullable(),
+});
+
+export const usageHelpSchemaLoose = z.object({
+	formType: z.object({ id: z.literal("USAGE_HELP") }),
+	softwareVersion: z.string().nullable(),
+	description: z.string().nullable(),
+});
+
+export const suggestionsSchemaLoose = z.object({
+	formType: z.object({ id: z.literal("SUGGESTIONS") }),
+	description: z.string().nullable(),
+});
+
+export const otherSchemaLoose = z.object({
+	formType: z.object({ id: z.literal("OTHER") }),
+	description: z.string().nullable(),
+});
+
+export const formSchemaLoose = z.object({
+	...commonFieldsLoose,
+	dedicatedFields: z
+		.union([
+			orderAndAccountIssuesSchemaLoose,
+			bugReportingSchemaLoose,
+			usageHelpSchemaLoose,
+			suggestionsSchemaLoose,
+			otherSchemaLoose,
+		])
+		.nullable(),
+});
+export type FormStateLoose = z.infer<typeof formSchemaLoose>;
